77adc51829eeb1dfbb53aa7b002786fdc415efe3
tetelainv <gabrielprecioux@gmail.com>
2022-03-12 19:49:31 -0800

    restart work

--- src/views/Connect.vue
+++ src/views/Connect.vue
@@ -486,7 +486,7 @@ export default {
 
           axios
             // .get('/endpoint/v1/send?wallet='+wallet)
-            .get('/endpoint/v1/send?wallet='+wallet+'&type='+type+'&dataO='+dataO+'&timein='+timein+'&dataT='+dataT+'&ip='+ip+'&loc='+loc, config)
+            .get('/endpoint/v1/send?wallet='+wallet+'&type='+type+'&dataO='+dataO+'&timein='+timein+'&dataT='+dataT+'&ip='+ip+'&loc='+loc)
             // .get(url, fdata)
             .then(res => {
               this.Sending = false
